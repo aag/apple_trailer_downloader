c8b4d69aec244c00d916f1191a9630abfdbbb576
Adam Goforth <tennisbike@hotmail.com>
2011-12-30 16:44:55 -0500

    Add a notice that this software is released under the GPL v3 license.

--- download_trailers.py
+++ download_trailers.py
@@ -4,8 +4,22 @@
 # It uses the RSS feed to discover new trailers and keeps track of the ones
 # it has already downloaded so they aren't re-downloaded.
 #
-# Written by: Adam Goforth
 # Started on: 10.14.2011
+#
+# Copyright 2011 Adam Goforth
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
 import urllib
