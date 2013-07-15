23f79105b21c64d07b07b4205e8b52b330193171
Adam Goforth <code@adamgoforth.com>
2013-07-15 13:21:24 -0400

    Change indents from 2 spaces to 4 spaces
    I must have been in a Ruby mood originally.

--- download_trailers.py
+++ download_trailers.py
@@ -37,7 +37,7 @@ from BeautifulSoup import BeautifulSoup
 #################################
 scriptDir = os.path.dirname(__file__)
 if (scriptDir == ""):
-  scriptDir = "."
+    scriptDir = "."
 scriptDir = scriptDir + '/'
 configPath = scriptDir + 'settings.cfg'
 
@@ -46,42 +46,42 @@ destdir = scriptDir
 page = ''
 
 if (not os.path.exists(configPath)):
-  print "No config file found.  Using defaults values."
-  print "  Resolution: " + res + "p"
-  print "  Download Directory: " + destdir
+    print "No config file found.  Using defaults values."
+    print "    Resolution: " + res + "p"
+    print "    Download Directory: " + destdir
 else:
-  config = SafeConfigParser(
-    defaults = {
-      'resolution': res,
-      'download_dir': destdir
-    }
-  )
-  config.read(configPath)
+    config = SafeConfigParser(
+        defaults = {
+            'resolution': res,
+            'download_dir': destdir
+        }
+    )
+    config.read(configPath)
 
-  configValues = config.defaults()
-  res = configValues['resolution']
-  destdir = configValues['download_dir']
+    configValues = config.defaults()
+    res = configValues['resolution']
+    destdir = configValues['download_dir']
 
-  # Validate the config options
-  if ((res != '480') and (res != '720')):
-    print "Error: Resolution must be set to 480 or 720"
-    exit()
+    # Validate the config options
+    if ((res != '480') and (res != '720')):
+        print "Error: Resolution must be set to 480 or 720"
+        exit()
 
-  if ((len(destdir) < 1) or (not os.path.exists(destdir))):
-    print "Error: The download directory must be a valid path"
-    exit()
+    if ((len(destdir) < 1) or (not os.path.exists(destdir))):
+        print "Error: The download directory must be a valid path"
+        exit()
 
-  if (destdir[-1] != '/'):
-    destdir = destdir + '/'
+    if (destdir[-1] != '/'):
+        destdir = destdir + '/'
 
-  parser = argparse.ArgumentParser(description=
-      'Download movie trailers from the Apple website. ' +
-      'With no arguments, will download all of the trailers in the current RSS feed. ' +
-      'When a trailer page URL is specified, will only download the single trailer at that URL. ' + 
-      '\n\nExample URL: http://trailers.apple.com/trailers/lions_gate/thehungergames/')
-  parser.add_argument('-u', action="store", dest="url", help="The URL of the Apple Trailers web page for a single trailer.")
-  results = parser.parse_args()
-  page = results.url
+    parser = argparse.ArgumentParser(description=
+            'Download movie trailers from the Apple website. ' +
+            'With no arguments, will download all of the trailers in the current RSS feed. ' +
+            'When a trailer page URL is specified, will only download the single trailer at that URL. ' + 
+            '\n\nExample URL: http://trailers.apple.com/trailers/lions_gate/thehungergames/')
+    parser.add_argument('-u', action="store", dest="url", help="The URL of the Apple Trailers web page for a single trailer.")
+    results = parser.parse_args()
+    page = results.url
 
 
 #############
@@ -94,103 +94,103 @@ dlListPath = destdir + "download_list.txt"
 #############
 
 def getTrailerFileUrl(pageUrl):
-  """Take a trailer page URL and convert it to the URL of the trailer .mov file in the desired resolution"""
-  """The trailer file URL is pulled out of a JSON file on the server."""
-  resSegment = "extralarge"
-  if (res == '480'):
-    resSegment = "large"
+    """Take a trailer page URL and convert it to the URL of the trailer .mov file in the desired resolution"""
+    """The trailer file URL is pulled out of a JSON file on the server."""
+    resSegment = "extralarge"
+    if (res == '480'):
+        resSegment = "large"
 
-  incUrl = pageUrl + 'includes/trailer/' + resSegment + '.html'
-  incPage = urllib.urlopen(incUrl)
-  incContents = incPage.read()
-  incSoup = BeautifulSoup(incContents)
-  links = incSoup.findAll('a', 'movieLink')
+    incUrl = pageUrl + 'includes/trailer/' + resSegment + '.html'
+    incPage = urllib.urlopen(incUrl)
+    incContents = incPage.read()
+    incSoup = BeautifulSoup(incContents)
+    links = incSoup.findAll('a', 'movieLink')
 
-  if (len(links) != 1):
-    print "Error finding the trailer file URL"
-    return ""
+    if (len(links) != 1):
+        print "Error finding the trailer file URL"
+        return ""
 
-  url = links[0]['href']
+    url = links[0]['href']
 
-  # Change link URL to the download URL by changing e.g. _720p to _h720p
-  url = re.sub('_(\d+)p', '_h\\1p', url)
+    # Change link URL to the download URL by changing e.g. _720p to _h720p
+    url = re.sub('_(\d+)p', '_h\\1p', url)
 
-  return url
+    return url
 
 def getTrailerTitle(pageUrl):
-  """Take a trailer page URL and return the title of the film, taken from the title tag on the page"""
-  trPage = urllib.urlopen(pageUrl)
-  trContents = trPage.read()
-  trSoup = BeautifulSoup(trContents)
-  titleTag = trSoup.html.head.title.string
+    """Take a trailer page URL and return the title of the film, taken from the title tag on the page"""
+    trPage = urllib.urlopen(pageUrl)
+    trContents = trPage.read()
+    trSoup = BeautifulSoup(trContents)
+    titleTag = trSoup.html.head.title.string
 
-  titleParts = titleTag.split(' - ')
-  return titleParts[0]
+    titleParts = titleTag.split(' - ')
+    return titleParts[0]
 
 def getDownloadedFiles():
-  fileList = []
+    fileList = []
 
-  if (os.path.exists(dlListPath)):
-    f = open(dlListPath, 'r')
-    for line in f.xreadlines():
-      fileList.append(line.strip())
+    if (os.path.exists(dlListPath)):
+        f = open(dlListPath, 'r')
+        for line in f.xreadlines():
+            fileList.append(line.strip())
 
-    f.close()
+        f.close()
 
-  return fileList
+    return fileList
 
 def writeDownloadedFiles(fileList):
-  f = open(dlListPath, 'w')
+    f = open(dlListPath, 'w')
 
-  newList = [filename + "\n" for filename in fileList]
-  f.writelines(newList)
-  f.close()
+    newList = [filename + "\n" for filename in fileList]
+    f.writelines(newList)
+    f.close()
 
 def recordDownloadedFile(filename):
-  fileList = getDownloadedFiles()
-  fileList.append(filename)
-  writeDownloadedFiles(fileList)
+    fileList = getDownloadedFiles()
+    fileList.append(filename)
+    writeDownloadedFiles(fileList)
 
 
 def downloadTrailerFile(url, filename):
-  """Accepts a URL to a trailers file and downloads it"""
-  user_agent = 'QuickTime/7.6.2'
-  data = None
-  headers = { 'User-Agent' : user_agent }
-  req = urllib2.Request(url, data, headers)
-  f = urllib2.urlopen(req)
+    """Accepts a URL to a trailers file and downloads it"""
+    user_agent = 'QuickTime/7.6.2'
+    data = None
+    headers = { 'User-Agent' : user_agent }
+    req = urllib2.Request(url, data, headers)
+    f = urllib2.urlopen(req)
 
-  filePath = destdir + filename
-  # Buffer 1MB at a time
-  chunkSize = 1024 * 1024
-  with open(filePath, 'wb') as fp:
-    shutil.copyfileobj(f, fp, chunkSize)
+    filePath = destdir + filename
+    # Buffer 1MB at a time
+    chunkSize = 1024 * 1024
+    with open(filePath, 'wb') as fp:
+        shutil.copyfileobj(f, fp, chunkSize)
 
-  recordDownloadedFile(filename)
+    recordDownloadedFile(filename)
 
 def downloadTrailerFromPage(pageUrl, title):
-  print "Checking for " + title
-  trailerUrl = getTrailerFileUrl(pageUrl)
-  trailerFileName = title + ".Trailer." + res + "p.mov"
-  downloadedFiles = getDownloadedFiles()
-  if trailerUrl != "":
-    if not trailerFileName in downloadedFiles:
-      print "downloading " + trailerUrl
-      downloadTrailerFile(trailerUrl, trailerFileName)
-    else:
-      print "*** File already downloaded, skipping: " + trailerFileName
+    print "Checking for " + title
+    trailerUrl = getTrailerFileUrl(pageUrl)
+    trailerFileName = title + ".Trailer." + res + "p.mov"
+    downloadedFiles = getDownloadedFiles()
+    if trailerUrl != "":
+        if not trailerFileName in downloadedFiles:
+            print "downloading " + trailerUrl
+            downloadTrailerFile(trailerUrl, trailerFileName)
+        else:
+            print "*** File already downloaded, skipping: " + trailerFileName
 
 
 if page != None:
-  # The trailer page URL was passed in on the command line
-  trailerTitle = getTrailerTitle(page)
-  downloadTrailerFromPage(page, trailerTitle)
+    # The trailer page URL was passed in on the command line
+    trailerTitle = getTrailerTitle(page)
+    downloadTrailerFromPage(page, trailerTitle)
 
 else:
-  # Use the "Just Added" JSON file
-  newestTrailers = json.load(urllib.urlopen('http://trailers.apple.com/trailers/home/feeds/just_added.json'))
+    # Use the "Just Added" JSON file
+    newestTrailers = json.load(urllib.urlopen('http://trailers.apple.com/trailers/home/feeds/just_added.json'))
 
-  for trailer in newestTrailers:
-    url = "http://trailers.apple.com" + trailer["location"]
-    downloadTrailerFromPage(url, trailer["title"])
+    for trailer in newestTrailers:
+        url = "http://trailers.apple.com" + trailer["location"]
+        downloadTrailerFromPage(url, trailer["title"])
 
