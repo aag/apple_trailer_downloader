c904becfff1bc4a9ce75f82650867d02cf9cfe87
Adam Goforth <code@adamgoforth.com>
2017-10-13 00:36:20 +0200

    Add more tests

--- test/test_download_trailers.py
+++ test/test_download_trailers.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 """This script contains tests for the Apple Trailers Downloader script.
 """
 
@@ -21,9 +23,12 @@
 import sys
 from os import path
 
+# Add the parent directory to the path so we can import the main script
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-
 import download_trailers as trailers
+
+
+TEST_DIR = test_dir = path.dirname(path.abspath(__file__))
 
 
 def test_map_res_to_apple_size_480():
@@ -36,3 +41,54 @@ def test_map_res_to_apple_size_720():
 
 def test_map_res_to_apple_size_1080():
     assert trailers.map_res_to_apple_size('1080') == 'hd1080'
+
+
+def test_convert_to_unicode():
+    assert trailers.convert_to_unicode('test') == u'test'
+
+
+def test_convert_src_url_to_file_url():
+    src_url = 'http://movietrailers.apple.com/movies/lionsgate/thehungergames/hungergames-tlr2_720p.mov'
+    file_url = 'http://movietrailers.apple.com/movies/lionsgate/thehungergames/hungergames-tlr2_h720p.mov'
+    assert trailers.convert_src_url_to_file_url(src_url, 720) == file_url
+
+
+def test_should_download_file_all():
+    assert trailers.should_download_file('all', '')
+    assert trailers.should_download_file('all', 'The Making of Safe and Sound')
+    assert trailers.should_download_file('all', 'Trailer')
+
+
+def test_should_download_file_single_trailer_trailer():
+    assert trailers.should_download_file('single_trailer', 'Trailer')
+
+
+def test_should_download_file_single_trailer_non_trailer():
+    assert not trailers.should_download_file('single_trailer', '')
+    assert not trailers.should_download_file('single_trailer', 'Clip')
+    assert not trailers.should_download_file('single_trailer', 'Sneak Peek')
+    assert not trailers.should_download_file('single_trailer', 'Trailer 2')
+
+
+def test_should_download_file_trailers_trailers():
+    assert trailers.should_download_file('trailers', 'Trailer')
+    assert trailers.should_download_file('trailers', 'Trailer 2')
+    assert trailers.should_download_file('trailers', 'Teaser')
+    assert trailers.should_download_file('trailers', 'Teaser 2')
+    assert trailers.should_download_file('trailers', 'First Look')
+
+
+def test_should_download_file_trailers_non_trailers():
+    assert not trailers.should_download_file('trailers', 'Clip')
+    assert not trailers.should_download_file('trailers', 'Sneak Peek')
+    assert not trailers.should_download_file('trailers', 'The Making of Safe and Sound')
+
+
+def test_get_downloaded_files_missing_file():
+    assert trailers.get_downloaded_files('/not/a/real/path/to/file.txt') == []
+
+
+def test_get_downloaded_files_existing_file():
+    list_file_path = path.join(TEST_DIR, 'fixtures', 'download_list.txt')
+    download_list = [u'Film.Trailer 2.1080p.mov', u'☃.Clip.480p.mov']
+    assert trailers.get_downloaded_files(list_file_path) == download_list
